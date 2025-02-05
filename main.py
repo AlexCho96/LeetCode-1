ded09a9cc8e7b6566fb614c79be2682f866a9a87
Alexandre Cho <alecho97@gmail.com>
2025-02-04 22:43:01 -0300

    First Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from solution import Solution
+
+def main():
+    sol = Solution()
+    print(sol.twoSum([2,7,11,15]))  # 2
+
+if __name__ == "__main__":
+    main()
---
+++ solution2.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if target - nums[i] in nums[i+1:]:
+                return [i,len(nums[:i+1])+nums[i+1:].index(target - nums[i])]
